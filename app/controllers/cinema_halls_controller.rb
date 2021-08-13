44d92a7020f747f05134c8c8e89b98f0eab2960a
Pawel Kozlik <pawel.kozlik@monterail.com>
2021-08-13 12:26:17 +0200

    refactor: move function from controller to model

--- app/controllers/cinema_halls_controller.rb
+++ app/controllers/cinema_halls_controller.rb
@@ -12,20 +12,20 @@ class CinemaHallsController < ApplicationController
   end
 
   def create
-    @cinema_hall = CinemaHall.new(cinema_hall_params)
-    if [20, 50, 100, 200].include? @cinema_hall.size
-      cinema_hall_rows_columns
-      render json: @cinema_hall, status: :created if @cinema_hall.save
+    cinema_hall = CinemaHall.new(cinema_hall_params)
+    if [20, 50, 100, 200].include? cinema_hall.size
+      before_save(cinema_hall)
+      render json: cinema_hall, status: :created if cinema_hall.save
     else
       render json: { error: 'Wrong size' }
     end
   end
 
   def edit
-    @cinema_hall = CinemaHall.find(params[:id])
-    if @cinema_hall.update(cinema_hall_params)
-      cinema_hall_rows_columns
-      render json: { success: 'Update successful' }
+    cinema_hall = CinemaHall.find(params[:id])
+    if cinema_hall.update(cinema_hall_params)
+      before_save(cinema_hall)
+      render json: cinema_hall, status: :created if cinema_hall.save
     else
       render json: @cinema_hall.errors
     end
@@ -41,20 +41,6 @@ class CinemaHallsController < ApplicationController
   end
 
   private
-
-  def cinema_hall_rows_columns
-    case @cinema_hall.size
-    when 20 then assign_column_and_rows(4, 5)
-    when 50 then assign_column_and_rows(5, 10)
-    when 100 then assign_column_and_rows(10, 10)
-    when 200 then assign_column_and_rows(20, 10)
-    end
-  end
-
-  def assign_column_and_rows(column_num, row_num)
-    @cinema_hall.column_num = column_num
-    @cinema_hall.row_num = row_num
-  end
 
   def cinema_hall_params
     params.permit(:name, :size)
