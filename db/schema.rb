99f01a54f6e72e1f3bf10bbd9a9f3b9710c05182
Pawel Kozlik <pawel.kozlik@monterail.com>
2021-08-23 11:19:15 +0200

    refactor: schema change after migrations

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,54 +10,56 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_818_115_855) do
+ActiveRecord::Schema.define(version: 2021_08_18_115855) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'cinema_halls', force: :cascade do |t|
-    t.integer 'name'
-    t.integer 'size'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "cinema_halls", force: :cascade do |t|
+    t.integer "name", null: false
+    t.integer "size", null: false
+    t.integer "column_num"
+    t.integer "row_num"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'movies', force: :cascade do |t|
-    t.string 'title'
-    t.integer 'length'
-    t.string 'director'
-    t.string 'genre'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "movies", force: :cascade do |t|
+    t.string "title", null: false
+    t.interval "length"
+    t.string "director"
+    t.string "genre"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'screenings', force: :cascade do |t|
-    t.bigint 'movie_id', null: false
-    t.bigint 'cinema_hall_id', null: false
-    t.datetime 'screen_time'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['cinema_hall_id'], name: 'index_screenings_on_cinema_hall_id'
-    t.index ['movie_id'], name: 'index_screenings_on_movie_id'
+  create_table "screenings", force: :cascade do |t|
+    t.bigint "movie_id", null: false
+    t.bigint "cinema_hall_id", null: false
+    t.datetime "screen_time", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["cinema_hall_id"], name: "index_screenings_on_cinema_hall_id"
+    t.index ["movie_id"], name: "index_screenings_on_movie_id"
   end
 
-  create_table 'seets', force: :cascade do |t|
-    t.string 'name', null: false
-    t.bigint 'cinema_hall_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['cinema_hall_id'], name: 'index_seets_on_cinema_hall_id'
+  create_table "seats", force: :cascade do |t|
+    t.string "name", null: false
+    t.bigint "cinema_hall_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["cinema_hall_id"], name: "index_seats_on_cinema_hall_id"
   end
 
-  create_table 'tickets', force: :cascade do |t|
-    t.string 'type', null: false
-    t.float 'price', null: false
-    t.boolean 'available', default: true
-    t.bigint 'seats_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['seats_id'], name: 'index_tickets_on_seats_id'
+  create_table "tickets", force: :cascade do |t|
+    t.string "ticket_type", null: false
+    t.float "price", null: false
+    t.bigint "screening_id", null: false
+    t.bigint "seat_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["screening_id"], name: "index_tickets_on_screening_id"
+    t.index ["seat_id"], name: "index_tickets_on_seat_id"
   end
 
-  add_foreign_key 'screenings', 'cinema_halls'
-  add_foreign_key 'screenings', 'movies'
 end
