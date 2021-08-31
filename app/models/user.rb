4e5759f393f45c15bac129da4e49e501a72ce3c4
Pawel Kozlik <pawel.kozlik@monterail.com>
2021-08-31 15:19:28 +0200

    refactor: delete omniauth from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@
 class User < ApplicationRecord
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :omniauthable
+         :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
   has_many :reservations, dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,3 +7,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#   bundle exec rails db:drop db:create db:migrate db:seed
+#   bundle exec rails c
+
+Movie.create(title: 'title', length: 'PT20S', director: 'director', genre: 'genre')
+CinemaHall.create(name: 1, size: 200, column_num: 20, row_num: 10)
+Screening.create(movie_id: 1, cinema_hall_id: 1, screen_time: '2021-08-25 11:25:06')
+Seat.create(name: 'name', cinema_hall_id: 1)
+Ticket.create(ticket_type: 'type', price: 10.99, screening_id: 1, seat_id: 1, reservation_id: 1)
+Reservation.create(status: 'status', screening_id: 1, user_id: 1)
+User.create(email: 'email@email.com', password: 'password', password_confirmation: 'password', role: 'role',
+            nickname: 'nickname', first_name: 'first_name', last_name: 'last_name')
